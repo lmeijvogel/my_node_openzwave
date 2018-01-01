0422bf921d5df2761e5934ec77ff874743784b0b
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2018-01-01 09:37:20 +0100

    Convert RedisInterface to class syntax.

--- redis_interface.js
+++ redis_interface.js
@@ -2,47 +2,41 @@
 
 const Redis = require('redis');
 
-function RedisInterface(commandChannel) {
-  let redis;
-
-  function start() {
-    const redisHost = process.env.REDIS_HOST || 'localhost';
-
-    redis = Redis.createClient(6379, redisHost);
+class RedisInterface {
+  constructor(commandChannel) {
+    this.commandChannel = commandChannel;
+    this.redis = null;
   }
 
-  function getVacationMode() {
-    return new Promise(function (resolve, reject) {
-      redis.hgetall('zwave_vacation_mode', function (err, values) {
+  start() {
+    const redisHost = process.env.REDIS_HOST || 'localhost';
+
+    this.redis = Redis.createClient(6379, redisHost);
+  }
+
+  getVacationMode() {
+    return new Promise((resolve, reject) => {
+      this.redis.hgetall('zwave_vacation_mode', (err, values) => {
         resolve(values || {state: 'off'});
       });
     });
   }
 
-  function vacationModeStarted(startTime, endTime) {
-    redis.hset('zwave_vacation_mode', 'state', 'on');
-    redis.hset('zwave_vacation_mode', 'start_time', startTime);
-    redis.hset('zwave_vacation_mode', 'end_time', endTime);
+  vacationModeStarted(startTime, endTime) {
+    this.redis.hset('zwave_vacation_mode', 'state', 'on');
+    this.redis.hset('zwave_vacation_mode', 'start_time', startTime);
+    this.redis.hset('zwave_vacation_mode', 'end_time', endTime);
   }
 
-  function vacationModeStopped() {
-    redis.hset('zwave_vacation_mode', 'state', 'off');
-    redis.hdel('zwave_vacation_mode', 'start_time');
-    redis.hdel('zwave_vacation_mode', 'end_time');
+  vacationModeStopped() {
+    this.redis.hset('zwave_vacation_mode', 'state', 'off');
+    this.redis.hdel('zwave_vacation_mode', 'start_time');
+    this.redis.hdel('zwave_vacation_mode', 'end_time');
   }
 
-  function cleanUp() {
-    redis.end();
+  cleanUp() {
+    this.redis.end();
   }
-
-  return {
-    start:                    start,
-    getVacationMode:          getVacationMode,
-    vacationModeStarted:      vacationModeStarted,
-    vacationModeStopped:      vacationModeStopped,
-    cleanUp:                  cleanUp
-  };
-
 }
 
 module.exports = RedisInterface;
