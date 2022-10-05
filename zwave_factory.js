8e37ae38fe3d8a9249010df8a2a68ae93696b8b0
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-10-05 08:59:09 +0200

    Always create a real OpenZWave library, even if it isn't used.
    This makes sure that the library is compiled (can be compiled). This might make
    depencency problems visible on the test server instead of live.

--- zwave_factory.js
+++ zwave_factory.js
@@ -1,23 +1,29 @@
 'use strict';
 
-const Logger = require('./logger');
+const Logger    = require('./logger');
+const OpenZWave = require('openzwave-shared');
+const FakeZWave = require('./fake_zwave');
 
 function ZWaveFactory(testMode) {
   function create() {
+    // Always create real instance, even if it's not going to be used.
+    // This makes sure that even in test mode, we can ascertain that the
+    // the real library can be compiled correctly, which saves panic fixes at 24:00.
+    const realZwave = new OpenZWave({
+      SaveConfig: true,
+      RetryTimeout: 3000
+    });
+
+    const fakeZWave = new FakeZWave();
+
     if (testMode) {
       Logger.info('ZWaveFactory: Creating Fake ZWave');
-      const FakeZWave = require('./fake_zwave');
 
-      return new FakeZWave();
+      return fakeZWave;
     } else {
       Logger.verbose('ZWaveFactory: Creating real ZWave');
 
-      const OpenZWave = require('openzwave-shared');
-
-      return new OpenZWave({
-        SaveConfig: true,
-        RetryTimeout: 3000
-      });
+      return realZwave;
     }
   }
 
