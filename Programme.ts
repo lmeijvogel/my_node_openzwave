02cb8406e6a9ce5acd51707091e0cd49ca7af6ea
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-10-05 08:59:12 +0200

    Improve factoring of Programme.ts.

--- Programme.ts
+++ Programme.ts
@@ -2,59 +2,39 @@ import { Logger } from "./Logger";
 import { ConfigLight } from "./ConfigLight";
 import { IMyZWave } from "./IMyZWave";
 
-class Action {
-  public readonly nodeName: string;
-  public readonly nodeid: number;
-  public readonly value: any;
-
-  constructor(nodeName: string, nodeId: number, value: any) {
-    this.nodeName = nodeName;
-    this.nodeid = nodeId;
-    this.value = value;
-  }
+export class Action {
+  constructor(readonly nodeName: string, readonly nodeId: number, readonly value: any) {}
 }
 
-interface IProgramme {
+export interface IProgramme {
   readonly name: string;
   apply(zwave: IMyZWave): void;
 }
 
-class Programme implements IProgramme {
-  public readonly name: string;
-
-  public readonly displayName: string;
+/* This does not need reloading because it will be replaced on reload:
+ * otherwise I can't easily account for disappearing and new programmes.
+ */
+export class Programme implements IProgramme {
   public readonly actions: Action[];
 
-  constructor(name: string, displayName: string, data: Map<string, object>, lights: ConfigLight[]) {
-    this.name = name;
-    this.displayName = displayName;
-    this.actions = [];
-
+  constructor(readonly name: string, readonly displayName: string, data: Map<string, object>, lights: ConfigLight[]) {
     Logger.debug(`Programme.constructor: Creating node with data: ${[...data]}, lights: ${[...lights]}`);
-    data.forEach((value, key) => {
-      const light = lights.find(l => l.name === key);
 
-      if (!light) {
-        throw new Error(`Error creating Programme "${this.name}": node "${key}" does not exist`);
-      }
-
-      const action = new Action(key, light.id, value);
-      this.actions.push(action);
-    });
+    this.actions = this.buildActions(data, lights, name);
   }
 
   apply(zwave: IMyZWave): void {
     this.actions.forEach(action => {
       try {
         if (action.value === true) {
-          Logger.verbose(`Send command "switch on" to node ${action.nodeid}`);
-          zwave.switchOn(action.nodeid);
+          Logger.verbose(`Send command "switch on" to node ${action.nodeId}`);
+          zwave.switchOn(action.nodeId);
         } else if (action.value === false) {
-          Logger.verbose(`Send command "switch off" to node ${action.nodeid}`);
-          zwave.switchOff(action.nodeid);
+          Logger.verbose(`Send command "switch off" to node ${action.nodeId}`);
+          zwave.switchOff(action.nodeId);
         } else {
-          Logger.verbose(`Send command "level ${action.value}" to node ${action.nodeid}`);
-          zwave.setLevel(action.nodeid, action.value);
+          Logger.verbose(`Send command "level ${action.value}" to node ${action.nodeId}`);
+          zwave.setLevel(action.nodeId, action.value);
         }
       } catch (e) {
         Logger.error(`ERROR in programme "${this.name}": Could not switch node "${action.nodeName}"`);
@@ -62,7 +42,21 @@ class Programme implements IProgramme {
       }
     });
   }
-}
 
-export { Programme };
-export { IProgramme };
+  private buildActions(data: Map<string, object>, lights: ConfigLight[], name: string): Action[] {
+    const result: Action[] = [];
+
+    data.forEach((value, key) => {
+      const light = lights.find(l => l.name === key);
+
+      if (!light) {
+        throw new Error(`Error creating Programme "${name}": node "${key}" does not exist`);
+      }
+
+      const action = new Action(key, light.id, value);
+      result.push(action);
+    });
+
+    return result;
+  }
+}
