13da9a8a94250da155f1dd06a400f7d77b966718
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-10-05 08:59:11 +0200

    Convert ConfigReader to class syntax.

--- config_reader.js
+++ config_reader.js
@@ -2,16 +2,12 @@
 
 const fs = require('fs');
 
-function ConfigReader() {
-  function read(filename) {
+class ConfigReader {
+  read(filename) {
     const contents = fs.readFileSync(filename, 'utf8');
 
     return JSON.parse(contents);
   }
-
-  return {
-    read: read
-  };
 }
 
 module.exports = ConfigReader;
