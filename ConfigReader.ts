8010f2ba8064e3eb2aa5b4edd1c7ae5def70db42
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-10-05 08:59:12 +0200

    Remove unused ConfigReader.

--- ConfigReader.ts
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-
-export class ConfigReader {
-    read(filename: string) {
-        const contents = readFileSync(filename, 'utf8');
-
-        return JSON.parse(contents);
-    }
-}
