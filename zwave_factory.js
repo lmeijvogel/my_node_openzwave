f747174c59297bdc7ef81f8e5253481420aae73c
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-10-05 08:59:10 +0200

    Decrease OpenZWave loglevel and don't save ZWave configuration.
    The previous loglevel was 'Detail' which was far too verbose.
    
    Saving the ZWave configuration sometimes results in the master
    switch presses not being recognized.

--- zwave_factory.js
+++ zwave_factory.js
@@ -4,14 +4,18 @@ const Logger    = require('./logger');
 const OpenZWave = require('openzwave-shared');
 const FakeZWave = require('./fake_zwave');
 
+const LOGLEVEL_INFO = 6;
+
 function ZWaveFactory(testMode) {
   function create() {
     // Always create real instance, even if it's not going to be used.
     // This makes sure that even in test mode, we can ascertain that the
     // the real library can be compiled correctly, which saves panic fixes at 24:00.
     const realZwave = new OpenZWave({
-      SaveConfig: true,
-      RetryTimeout: 3000
+      SaveConfiguration: false,
+      RetryTimeout: 3000,
+      SaveLogLevel: LOGLEVEL_INFO,
+      QueueLogLevel: LOGLEVEL_INFO
     });
 
     const fakeZWave = new FakeZWave();
